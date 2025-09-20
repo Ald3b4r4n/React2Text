b9efc4a115f2946275420930a8f31d74bb250a4c
Ald3b4r4n <rafasouzacruz@gmail.com>
2025-09-20 16:54:12 -0300

    novos arquivos

---
+++ react2text/public/sw.js
@@ -0,0 +1,27 @@
+// Service Worker simples para PWA
+const CACHE_NAME = "doc2text-v1";
+const urlsToCache = [
+  "/",
+  "/static/js/bundle.js",
+  "/static/css/main.css",
+  "/icon-192x192.png",
+  "/icon-512x512.png",
+  "/manifest.json",
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      if (response) {
+        return response;
+      }
+      return fetch(event.request);
+    })
+  );
+});
